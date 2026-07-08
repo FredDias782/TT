23764fb6921afb03759342b7be56231642d68cfe
GGL24 <ggoncalves.l24@us.edu.cv>
2026-07-08 21:46:57 -0100

    Add getters, setters, and toString method in No class
    Added getters and setters for musica, anterior, and proximo attributes, and overridden toString method.

--- No.java
+++ No.java
@@ -8,4 +8,34 @@ public class No {
         this.anterior = null;
         this.proximo = null;
     }
+     // Getters
+    public String getMusica() {
+        return musica;
+    }
+
+    public No getAnterior() {
+        return anterior;
+    }
+
+    public No getProximo() {
+        return proximo;
+    }
+    // Setters
+    public void setMusica(String musica) {
+        this.musica = musica;
+    }
+
+    public void setAnterior(No anterior) {
+        this.anterior = anterior;
+    }
+
+    public void setProximo(No proximo) {
+        this.proximo = proximo;
+    }
+     @Override
+    public String toString() {
+        return "No{" +
+                "musica='" + musica + '\'' +
+                '}';
+    }
 }
